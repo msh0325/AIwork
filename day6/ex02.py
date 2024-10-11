bffd042782e526c83d394e171ad172576e7df809
msh0325 <msh0325@daum.net>
2024-10-11 15:05:02 +0900

    day6_ex02

--- day6/ex02.py
+++ day6/ex02.py
@@ -5,3 +5,10 @@ import numpy as np
 gate_data = np.loadtxt("D:\\aiwork_msh\\AIwork\\day6\\gate.csv",delimiter=',',dtype=np.int8,skiprows=1)
 print(gate_data)
 # %%
+print(gate_data[:,0:2])
+# %%
+print("and : ",gate_data[:,2])
+# %%
+print("or : ",gate_data[:,3])
+print("xor : ",gate_data[:,4])
+# %%
