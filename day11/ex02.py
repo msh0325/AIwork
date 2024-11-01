f8ec5d7e5f0d667d015a4d88c4cfb3702812f132
msh0325 <msh0325@daum.net>
2024-11-01 15:42:33 +0900

    day11_2

--- day11/ex02.py
+++ day11/ex02.py
@@ -64,3 +64,53 @@ print(x_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 # %%
+class LottoLSTM(nn.Module) :
+    def __init__ (self,input_size,hidden_size,output_size,num_layers):
+        super(LottoLSTM,self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size,hidden_size,num_layers,batch_first=True)
+        self.fc = nn.Linear(hidden_size,output_size)
+    
+    def forward(self,x) : 
+        h0 = torch.zeros(self.num_layers,x.size(0),self.hidden_size)
+        c0 = torch.zeros(self.num_layers,x.size(0),self.hidden_size)
+        out = self.lstm(x,(h0,c0))
+        out = self.fc(out[:,-1,:])
+        return out
+    
+model = LottoLSTM(7,64,7,2)
+# %%
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(),lr = 0.001)
+# %% train
+num_epochs = 1000
+for epoch in range(num_epochs) :
+    model.train()
+    outputs = model(x_train)
+    loss = loss_fn(outputs,y_train)
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    if epoch %100 == 0 :
+        print(f"epoch [{epoch}/{num_epochs}], loss {loss.item():.4f}")
+# %% test
+model.eval()
+with torch.no_grad() :
+    test_outputs = model(x_test)
+    test_loss = loss_fn(test_outputs,y_test)
+    print(f'Test loss {test_loss.item():.4f}')
+# %% 어떤 번호를 찍어주는지 출력
+x_tensor = torch.tensor(x,dtype=torch.float32)
+last_sequence = x_tensor[-1].unsqueeze(0)
+print(last_sequence)
+# %%
+with torch.no_grad() :
+    next_pred = model(last_sequence)
+
+# %%
+pre_nums = scaler.inverse_transform(next_pred.detach().numpy())
+print(np.round(pre_nums[0]).astype(int)+1)
+# %%
